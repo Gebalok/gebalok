2223c9323bd2a592ecf7ee83d718b2734edd2ac1
Gebalok <9218222@stud.kai.edu.ua>
2025-11-17 11:17:02 +0300

    Update Program.cs

--- EchoTspServer/Program.cs
+++ EchoTspServer/Program.cs
@@ -12,7 +12,7 @@ namespace EchoServer
     {
         private readonly int _port;
         private TcpListener _listener;
-        private CancellationTokenSource _cancellationTokenSource;
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
 
         //constuctor
         public EchoServer(int port)
